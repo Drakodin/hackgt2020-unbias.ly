ad1fb3901055e55487e102ebf33b4a6cac08ce67
Drakodin <tony.y.tian@gmail.com>
2020-10-17 12:20:08 -0500

    Set up UI structures. Remove bootstrapped.

--- ui/src/routes/Router.js
+++ ui/src/routes/Router.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { Home } from '../home';
 
 const Router = () => {
     return (
-        <BrowserRouter>
+        <Switch>
             <Route exact path='/' component={Home}/>
-        </BrowserRouter>
+        </Switch>
     )
 }
+export default Router;
