a1fa1628f8f3871f0542d89d482b5c221e8dc7ec
Aditya Singhal <57043781+ATANKERA@users.noreply.github.com>
2020-10-17 18:25:47 -0500

    Add files via upload

--- api/ranker/scapertest.py
+++ api/ranker/scapertest.py
@@ -12,102 +12,104 @@ headers = {
     }
 
 
+def scraping:
+    #NDTV
+    # --------------------------------------------------------------------------------------------
+    url = "https://www.ndtv.com/topic/politics"
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    #print(soup.prettify())
+    #print(soup.find_all("a"))
+    articles = soup.find_all("a")
+    #articles = articles[10:50]
+    #print(articles)
+    artlinks = []
 
-#NDTV
-# --------------------------------------------------------------------------------------------
-url = "https://www.ndtv.com/topic/politics"
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-#print(soup.prettify())
-#print(soup.find_all("a"))
-articles = soup.find_all("a")
-#articles = articles[10:50]
-#print(articles)
-artlinks = []
+    for a in articles:
+        artlinks+=[a.get('href')]
 
-for a in articles:
-    artlinks+=[a.get('href')]
+    #print(artlinks)
+    #print(artlinks.index("https://www.ndtv.com/india-news/congress-had-stepmotherly-attitude-towards-ladakh-for-70-years-g-kishan-reddy-2311747"))
 
-#print(artlinks)
-#print(artlinks.index("https://www.ndtv.com/india-news/congress-had-stepmotherly-attitude-towards-ladakh-for-70-years-g-kishan-reddy-2311747"))
-
-NDTVLinks = artlinks[50:77]
-#print(links)
-# -------------------------------------------------------------------------------------------
-
-
-
-# INDIAN EXPRESS
-# --------------------------------------------------------------------------------------------
-url = "https://indianexpress.com/section/india/politics/"
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-# print(soup.find_all("a"))
-
-articles = soup.find_all("a")
-# print(articles)
-artlinks = []
-
-for a in articles:
-    artlinks+=[a.get('href')]
-
-# print(artlinks)
-# print(artlinks.index("https://indianexpress.com/article/india/politics/maharashtra-agriculture-water-supply-sharad-pawar-baramati-indapur-5766457/"))
-
-IndianExpressLinks = artlinks[37:62]
-#print(links)
-# --------------------------------------------------------------------------------------------
+    NDTVLinks = artlinks[50:77]
+    #print(links)
+    # -------------------------------------------------------------------------------------------
 
 
 
-# DECCAN CHRONICLES
-# --------------------------------------------------------------------------------------------
-url = "https://www.deccanchronicle.com/nation/politics"
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-# print(soup.find_all("a"))
+    # INDIAN EXPRESS
+    # --------------------------------------------------------------------------------------------
+    url = "https://indianexpress.com/section/india/politics/"
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    # print(soup.find_all("a"))
 
-articles = soup.find_all("a")
-# print(articles)
-artlinks = []
+    articles = soup.find_all("a")
+    # print(articles)
+    artlinks = []
 
-for a in articles:
-    artlinks+=["https://www.deccanchronicle.com" + a.get('href')]
+    for a in articles:
+        artlinks+=[a.get('href')]
 
-# print(artlinks)
-DeccanLinks = artlinks[123: 163]
+    # print(artlinks)
+    # print(artlinks.index("https://indianexpress.com/article/india/politics/maharashtra-agriculture-water-supply-sharad-pawar-baramati-indapur-5766457/"))
+
+    IndianExpressLinks = artlinks[37:62]
+    #print(links)
+    # --------------------------------------------------------------------------------------------
 
 
-# --------------------------------------------------------------------------------------------
+
+    # DECCAN CHRONICLES
+    # --------------------------------------------------------------------------------------------
+    url = "https://www.deccanchronicle.com/nation/politics"
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    # print(soup.find_all("a"))
+
+    articles = soup.find_all("a")
+    # print(articles)
+    artlinks = []
+
+    for a in articles:
+        artlinks+=["https://www.deccanchronicle.com" + a.get('href')]
+
+    # print(artlinks)
+    DeccanLinks = artlinks[123: 163]
 
 
+    # --------------------------------------------------------------------------------------------
 
 
 
 
-LINKS = IndianExpressLinks+NDTVLinks+DeccanLinks
 
-ARTICLES = []
 
-for l in LINKS:
-    url = l
-    art = Article(url)
-    art.download()
-    art.parse()
-    ARTICLES +=[art.text]
+    LINKS = IndianExpressLinks+NDTVLinks+DeccanLinks
 
-# VADER Scores, specifically compound
-SCORES = []
-analyzer = SentimentIntensityAnalyzer()
-for A in ARTICLES:
-    vs = analyzer.polarity_scores(A)
-    SCORES += [abs(vs['compound'])]
+    ARTICLES = []
 
-LINKS_FINAL = []
-for i in range(len(LINKS)):
-    if SCORES[i] != 0:
-        value = {'link': LINKS[i], 'score': SCORES[i]}
-        if value in LINKS_FINAL:
-            continue
-        else:
-            LINKS_FINAL += [value]
+    for l in LINKS:
+        url = l
+        art = Article(url)
+        art.download()
+        art.parse()
+        ARTICLES +=[art.text]
+
+    # VADER Scores, specifically compound
+    SCORES = []
+    analyzer = SentimentIntensityAnalyzer()
+    for A in ARTICLES:
+        vs = analyzer.polarity_scores(A)
+        SCORES += [abs(vs['compound'])]
+
+    LINKS_FINAL = []
+    for i in range(len(LINKS)):
+        if SCORES[i] != 0:
+            value = {'link': LINKS[i], 'score': SCORES[i]}
+            if value in LINKS_FINAL:
+                continue
+            else:
+                LINKS_FINAL += [value]
+
+    return LINKS_FINAL
